3c4a80ce310079411e985c26b98deb8738e19b58
Terje Christensen <terchris@users.noreply.github.com>
2023-01-17 12:26:29 +0100

    testing more

--- shadow-brreg-setup.sh
+++ shadow-brreg-setup.sh
@@ -14,7 +14,7 @@
 apk update && apk add libreoffice
 
 # download database table definition from github
-wget https://raw.githubusercontent.com/terchris/shadow-brreg/main/brreg_enheter_alle-table_definition.sql
+wget https://raw.githubusercontent.com/terchris/shadow-brreg/main/brreg_enheter_alle-table_definition.sql -O /tmp/brreg_enheter_alle-table_definition.sql
 
 # 2 create database in the container
 PGPASSWORD="postgres" psql -p 5433 --user=postgres -c "CREATE DATABASE importdata OWNER postgres;"
@@ -23,27 +23,34 @@ PGPASSWORD="postgres" psql -p 5433 --user=postgres -c "CREATE DATABASE importdat
 ## delete previous downloaded table
 PGPASSWORD="postgres" psql -p 5433 -d importdata --user=postgres -c "DROP TABLE brreg_enheter_alle;"
 ## delete downloaded excel file
-rm enheter_alle.xlsx
+rm /tmp/enheter_alle.xlsx
 ## delete converted cvs file 
-rm enheter_alle.csv
+rm /tmp/enheter_alle.csv
 
 
 # 4 create the database table using the definition file
-PGPASSWORD="postgres" psql -p 5433 -U postgres -d importdata -f brreg_enheter_alle-table_definition.sql
+PGPASSWORD="postgres" psql -p 5433 -U postgres -d importdata -f /tmp/brreg_enheter_alle-table_definition.sql
 
 # 5 download excel file enheter_alle.xlsx from brreg.no 
-wget --header='Accept: application/vnd.brreg.enhetsregisteret.enhet+vnd.openxmlformats-officedocument.spreadsheetml.sheet;charset=UTF-8' -O enheter_alle.xlsx 'https://data.brreg.no/enhetsregisteret/api/enheter/lastned/regneark'
+wget --header='Accept: application/vnd.brreg.enhetsregisteret.enhet+vnd.openxmlformats-officedocument.spreadsheetml.sheet;charset=UTF-8' -O /tmp/enheter_alle.xlsx 'https://data.brreg.no/enhetsregisteret/api/enheter/lastned/regneark'
 
 # 6 Convert excel file enheter_alle.xlsx to csv format and name it enheter_alle.csv
-soffice --headless --convert-to csv:"Text - txt - csv (StarCalc)":44,34,76 enheter_alle.xlsx
+soffice --headless --convert-to csv:"Text - txt - csv (StarCalc)":44,34,76 /tmp/enheter_alle.xlsx
 
 # 7 import csv file enheter_alle.csv into database
-PGPASSWORD="postgres" psql -p 5433 -d importdata --user=postgres -c "\COPY brreg_enheter_alle FROM './enheter_alle.csv' delimiter ',' csv header;"
+PGPASSWORD="postgres" psql -p 5433 -d importdata --user=postgres -c "\COPY brreg_enheter_alle FROM '/tmp/enheter_alle.csv' delimiter ',' csv header;"
 
 # 8 delete downloaded files
-rm enheter_alle.xlsx
-rm enheter_alle.csv
-rm brreg_enheter_alle-table_definition.sql
+rm /tmp/enheter_alle.xlsx
+rm /tmp/enheter_alle.csv
+rm /tmp/brreg_enheter_alle-table_definition.sql
+rm /tmp/shadow-brreg-setup.sh 
 
 # 9 create a file so that that file has the date and time of the last update
 date > last_update.txt
+
+# 10 add the number of records imported to the file last_update.txt
+PGPASSWORD="postgres" psql -p 5433 -d importdata --user=postgres -c "select count(*) from brreg_enheter_alle;" >> last_update.txt
+
+# 11 display the content of the file last_update.txt
+cat last_update.txt
