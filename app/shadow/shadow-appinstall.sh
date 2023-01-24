f26ee4856025b1ff79a50899bda0010c3197fcae
Terje Christensen <terchris@users.noreply.github.com>
2023-01-24 15:25:53 +0100

    nyt forsøk

--- app/shadow/shadow-appinstall.sh
+++ app/shadow/shadow-appinstall.sh
@@ -3,6 +3,12 @@
 # code for installing the shadow app
 
 echo "shadow-appinstall.sh"
+git clone --no-hardlinks https://github.com/terchris/shadow-brreg  
+
+# make scripts executable
+chmod +x /shadow-brreg/app/shadow/shadow-appinstall.sh
+chmod +x /shadow-brreg/app/shadow/shadow-cronjob.sh
+chmod +x /shadow-brreg/app/shadow/shadow-cronstart.sh
 
 
 echo "Set up and compile the shadow app"
@@ -14,12 +20,11 @@ ls ./dist -la
 
 #yarn initdb
 
-#echo "Set up the cron job"
-#crontab -l > mycron
-#echo "*/1 * * * * /shadow-brreg/app/shadow/shadow-cron.sh" >> mycron
-#crontab mycron
-#rm mycron
+echo "Set up the cron job"
+/usr/bin/crontab /shadow-brreg/app/shadow/cronjobs.txt
 
+echo "Start the cron job"
+/usr/sbin/crond -f -l 8
 
 
 echo "shadow-setup.sh done"
